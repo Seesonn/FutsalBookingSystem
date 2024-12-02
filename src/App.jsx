0580fa314e20af7c9bf8bd9d6d487f6a262a4049
Sisan Bhattarai <125193929+Seesonn@users.noreply.github.com>
2024-12-02 11:31:17 +0545

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import React from "react"
 import Home from "./project/Home"
 import About from "./project/About"
 import Contact from "./project/Contact"
-import Footer from "./project/Footer"
-
+import Footer from "./project/Footer"import FutsalVenues from "./project/Futsal"
 function App() {
  
 
@@ -11,6 +10,7 @@ function App() {
     <div className="bg-[#04153F]">
     <Home/>
     <About/>
+    <FutsalVenues/>
     <Contact/>
     <Footer/>
    
