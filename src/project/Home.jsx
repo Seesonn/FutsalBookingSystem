3da42cb01fe74256dac7e818e837374905517e6d
Sisan Bhattarai <125193929+Seesonn@users.noreply.github.com>
2024-12-04 21:45:28 +0545

    Update Home.jsx

--- src/project/Home.jsx
+++ src/project/Home.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
 
-import Navigation from './Navigation';
 import Hero from './Hero'
 function Home() {
   return (
     <div id="home" className="container mx-auto px-4 text-white bg-[#04153F] font-roboto">
       
       
-      <Navigation/>
+     
       <Hero/>
      
     </div>
