240f4b9f9aada37e7f53d5a39c88b34bfd9af80e
Sisan Bhattarai <125193929+Seesonn@users.noreply.github.com>
2024-12-04 21:32:01 +0545

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,39 @@
-import React from "react"
-import Home from "./project/Home"
-import About from "./project/About"
-import Contact from "./project/Contact"
-import Footer from "./project/Footer"
-import FutsalVenues from "./project/Futsal"
-import BookingSlot from "./project/BookingSlots"
-function App() {
- 
 
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navigation from "./project/Navigation";
+import Home from "./project/Home";
+import About from "./project/About";
+import Contact from "./project/Contact";
+import Footer from "./project/Footer";
+import VerificationCode from "./project/VerificationCode";
+import Login from "./project/Login";
+import Register from "./project/Register";
+import FutsalCardPage from "./project/FutsalcardPage";
+import BookingSlot from "./project/BookingSlots";
+
+function App() {
   return (
-    <div className="bg-[#04153F]">
-    <Home/>
-    <About/>
-    <FutsalVenues/>
-    <BookingSlot/>
-    <Contact/>
-    <Footer/>
-   
-    </div>
-  )
+    <Router>
+      <div className="bg-[#04153F] min-h-screen flex flex-col">
+        <Navigation />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/futsal-venues" element={<FutsalCardPage/>} />
+            <Route path="/booking" element={<BookingSlot />} />
+            <Route path="/verify" element={<VerificationCode />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
